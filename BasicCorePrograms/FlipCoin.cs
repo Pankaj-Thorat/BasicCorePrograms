db48e76261c2dad4777b6f44be7ffd9980823b72
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-20 23:38:20 +0530

    [Pankaj]  Improved readability

--- BasicCorePrograms/FlipCoin.cs
+++ BasicCorePrograms/FlipCoin.cs
@@ -12,13 +12,14 @@ namespace BasicCorePrograms
         {
             int heads = 0;
             int tails = 0;
-
+            //to get input from user for specific no coin flips
             Console.WriteLine("Enter number of times to flip coin:");
             int flips = Convert.ToInt32(Console.ReadLine());
-
+            
             Random rndm = new Random();
             for (int i = 0; i < flips; i++)
             {
+                //to generate random value from min 0 to max 1
                 int result = rndm.Next(2);
                 if (result == 0)
                 {
@@ -31,10 +32,10 @@ namespace BasicCorePrograms
                     tails++;
                 }
             }
-
+            //formula for percentage
             double headsPercentage = (double)heads / flips * 100;
             double tailsPercentage = (double)tails / flips * 100;
-
+            //output
             Console.WriteLine("Percentage of heads: {0}%", headsPercentage);
             Console.WriteLine("Percentage of tails: {0}%", tailsPercentage);
         }
