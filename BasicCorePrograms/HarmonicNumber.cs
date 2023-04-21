1771e142b2b07bbeba262dd091a106e0b0c76eea
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-21 10:55:39 +0530

    [Pankaj] Calculate Harmonic Number

---
+++ BasicCorePrograms/HarmonicNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    class HarmonicNumber
+    {
+        
+        public static void CalculateHarmonicNumber()
+        { 
+            int i, n;
+            double sum = 0.0;
+            //input from user 
+            Console.WriteLine("Enter the Number of terms");
+            n = Convert.ToInt32(Console.ReadLine());
+            //logic for addition of harmonic number
+            for (i = 1; i <= n; i++)
+            {
+                sum += 1 /(float)i;
+            }
+            Console.WriteLine("Sum of {0} Harmonic Numbers is {1}",n,sum);
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -8,13 +8,14 @@ namespace BasicCorePrograms
         {
             //getting input from user to execute specific code
             Console.WriteLine("Please choose your program");
-            Console.WriteLine("1.FlipCoin \n2.Leap Year");
+            Console.WriteLine("1.FlipCoin \n2.Leap Year \n3.Harmonic Number");
             int option = int.Parse(Console.ReadLine());
 
             switch (option)
             {
                 case 1: FlipCoin.FlipTheCoin(); break;
                 case 2: LeapYear.CheckLeapYear();break;
+                case 3: HarmonicNumber.CalculateHarmonicNumber();break;
             }
 
         }
